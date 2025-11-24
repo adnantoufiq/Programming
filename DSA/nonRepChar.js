7dadb3260b7d47b7c8c928ae47e4ab5f576ab40b
toufiq <aiful.toufiq@technonext.com>
2025-11-24 10:13:07 +0600

    non-rep-char

---
+++ DSA/nonRepChar.js
@@ -0,0 +1,28 @@
+function nonRepChar(str) {
+    const n = char.length
+    const map = new Map()
+    let newStr = ''
+
+    for (let i = 0 ; i<n; i++)
+    {
+        const ch = char[i]
+        if(map.has(ch)){
+            map.set(ch, map.get(ch) + 1)
+            break
+        } else {
+            map.set(ch, 1)
+            newStr += ch
+        }
+    }
+   
+    let count = newStr.length
+
+    return {newStr, count}
+
+
+}
+
+
+const char = 'abcdab';
+
+console.log(nonRepChar(char));
